125c59defbb467928f0184f28257d76efd87c0e0
EC2 Default User <doogadavid3@gmail.com>
2025-12-27 11:10:45 +0000

    modified test app

--- test_app.py
+++ test_app.py
@@ -1,3 +1,12 @@
+import pytest
+from app import app as flask_app
+
+@pytest.fixture
+def client():
+    
+    with flask_app.test_client() as client:
+        yield client
+
 def test_homepage_status_code(client):
     response = client.get('/')
     assert response.status_code == 200
